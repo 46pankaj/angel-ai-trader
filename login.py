091c31036bda443e6ecd0a642c24eb093bfc025c
46pankaj <36914172+46pankaj@users.noreply.github.com>
2025-04-16 23:30:11 +0530

    Delete login.py

--- login.py
+++
@@ -1,7 +0,0 @@
-from smartapi import SmartConnect
-import config
-
-def angel_login():
-    obj = SmartConnect(api_key=config.API_KEY)
-    session = obj.generateSession(config.CLIENT_ID, config.PIN, config.TOTP)
-    return obj, session['data']['jwtToken'], session['data']['feedToken']
